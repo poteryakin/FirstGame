69e9b1480ca45da7dddeeee22e17a10c5e2faad1
Ilya Poteryaev <poteryaev.ilya2001@gmail.com>
2024-12-02 11:01:54 +0300

    update game

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const btns = document.querySelectorAll('button');
+let result = document.getElementById("result");
+btns.forEach(b => b.addEventListener("click", playRound));
+
 function getComputerChoice() {
     let random_number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     let str;
@@ -9,62 +13,57 @@ function getComputerChoice() {
     return str.toLowerCase();
 }
 
-function getHumanChoice() {
-    let human_choice = prompt('Введите ваш выбор', 'Ножницы');
-    return human_choice.toLowerCase();
+function getHumanChoice(e) {
+    return this.value;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 'бумага') {
-        if (computerChoice == 'камень') {
-            ++humanScore;
-            return 'win';
+function playRound() {
+    let humanChoice = this.value;
+    let computerChoice = getComputerChoice();
+    if (computerScore != 5 || humanScore != 5) {
+        if (humanChoice == 'бумага') {
+            if (computerChoice == 'камень') {
+                ++humanScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else if (computerChoice == 'ножницы') {
+                ++computerScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else {
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
         }
-        else if (computerChoice == 'ножницы') {
-            ++computerScore;
-            return 'los';
+        if (humanChoice == 'ножницы') {
+            if (computerChoice == 'камень') {
+                ++computerScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else if (computerChoice == 'ножницы') {
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else {
+                ++humanScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
         }
-        else {
-            return 'draw';
+        if (humanChoice == 'камень') {
+            if (computerChoice == 'камень') {
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else if (computerChoice == 'ножницы') {
+                ++humanScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
+            else {
+                ++computerScore;
+                result.innerText = `Computer: ${computerScore} vs You: ${humanScore}`;
+            }
         }
     }
-    if (humanChoice == 'ножницы') {
-        if (computerChoice == 'камень') {
-            ++computerScore;
-            return 'loss';
-        }
-        else if (computerChoice == 'ножницы') {
-            return 'draw';
-        }
-        else {
-            ++humanScore;
-            return 'win';
-        }
+    if (computerScore == 5 || humanScore == 5) {
+        (humanScore == 5) ? result.innerText = `You win` : result.innerText = `You loss`;
+        humanScore = 0;
+        computerScore = 0;
     }
-    if (humanChoice == 'камень') {
-        if (computerChoice == 'камень') {
-            return 'draw';
-        }
-        else if (computerChoice == 'ножницы') {
-            ++humanScore;
-            return 'win';
-        }
-        else {
-            ++computerScore;
-            return 'loss';
-        }
-    }
-}
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-    let resultHuman = humanScore;
-    let resultComputer = computerScore;
-    humanScore = 0;
-    computerScore = 0;
-    return `Result: Computer - ${resultComputer} and You - ${resultHuman}`; 
 }
